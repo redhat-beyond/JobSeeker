344ce58588a9f04c89c33ff0f4529e534a8b71c6
paOmer <omerpahima7@gmail.com>
2022-03-14 11:01:44 +0200

    Setting the feed page to be the homepage of the project

--- app/settings.py
+++ app/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'jobseeker.apps.JobseekerConfig',
-    'django.contrib.staticfiles'
+    'django.contrib.staticfiles',
+    'feed.apps.FeedConfig',
 ]
 
 MIDDLEWARE = [
