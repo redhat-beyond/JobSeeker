3a3cebc3dd451cf7e0dab50b0e0d3696802182b5
paOmer <omerpahima7@gmail.com>
2022-03-15 19:09:20 +0200

    Setting up the about tab

--- jobseeker/views.py
+++ jobseeker/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 
 # Create your views here.
-def index(request):
-    return render(request, 'index.html')
+def about(request):
+    return render(request, 'about.html', {'title': 'about'})
