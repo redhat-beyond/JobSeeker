d76e468066c3eef19f44175d22070e518efe0397
ShirleyFichman <shirleyfichman@gmail.com>
2022-03-16 19:28:56 +0200

    merge conflict in app/urls.py

--- app/urls.py
+++ app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('feed.urls')),
     path('jobseeker/', include('jobseeker.urls')),
+    path('profile/', include('personal_profile.urls')),
 ]
