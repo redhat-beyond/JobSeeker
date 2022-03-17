f38b04171d583ce18b90db1419d728cc4bec08d2
ShirleyFichman <shirleyfichman@gmail.com>
2022-03-17 14:23:25 +0200

    Adding models and migrations to the personal profile

--- personal_profile/models.py
+++ personal_profile/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class ProfileManager(models.Manager):
+    def main_profile(self):
+        return self
+
+
+class PersonalProfile(models.Model):
+    company= models.CharField(max_length=100)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    about= models.TextField() 
+  #  profile_picture= 
+  #  resume=
+  
+    def __str__(self):
+      return f'{self.user.username} Profile'
+
--- personal_profile/views.py
+++ personal_profile/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from personal_profile.models import PersonalProfile
 
 def personal_profile(request):
-    return render(request, 'personal_profile/personal_profile.html')
+    context = {
+        'personal_profile': PersonalProfile
+    }
+    return render(request, 'personal_profile/personal_profile.html', context)
