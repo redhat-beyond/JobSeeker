5cfe5e66a769b19b98912a3d826da2e653246860
Dean <dinbiton100@gmail.com>
2022-03-21 00:28:08 +0200

    Adding data models for the job_board app
    Data models added:
    - JobType
    - Location
    - YearsOfExperience
    - Preference

---
+++ job_board/models/location.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Location(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+    def __str__(self):
+        return self.name
---
+++ job_board/models/preference.py
@@ -0,0 +1,10 @@
+from django.db import models
+from .job_type import JobType
+from .location import Location
+from .years_of_experience import YearsOfExperience
+
+
+class Preference(models.Model):
+    job_type = models.ForeignKey(JobType, on_delete=models.RESTRICT)
+    location = models.ForeignKey(Location, on_delete=models.RESTRICT)
+    years_of_experience = models.ForeignKey(YearsOfExperience, on_delete=models.RESTRICT)
