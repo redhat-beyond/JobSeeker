4d33362182997e57c894f56cbdffbf7a71dab814
ShirleyFichman <shirleyfichman@gmail.com>
2022-03-28 17:14:08 +0300

    Adding tests to the data model
    Changing NULL to NONE and deleting non-relevant libraries
    
    changing the path of the profile pic test
    
    changing the path of the profile pic test
    
    changing the path of the profile pic test
    
    changing the path of the profile pic test
    
    changing the path of the profile pic test
    
    correcting typing err"

--- personal_profile/tests.py
+++ personal_profile/tests.py
@@ -1,6 +1,37 @@
 import pytest
 import django.contrib.auth
+from .models import PersonalProfile
+from django.core.files.uploadedfile import SimpleUploadedFile
+
+
+IMAGE_PATH = 'personal_profile/static/personal_profile/images/test_image.jpg'
+User = django.contrib.auth.get_user_model()
 
 def test_profile_app_entrypoint(client):
     response = client.get("/profile/")
     assert response.status_code == 200
+
+@pytest.fixture()
+def user_1(db):
+    return User.objects.create_user('user_1', password='userpassword')
+
+@pytest.fixture()
+def profile_1(db, user_1):
+    profile_1 = PersonalProfile(company = 'Test Company', user = user_1, about = 'Test About',
+                                birth_date = '2006-10-25', profile_pic = SimpleUploadedFile(name='test_image.jpg',
+                                content=open(IMAGE_PATH, 'rb').read(), content_type='image/jpeg'),
+                                resume= SimpleUploadedFile('test_resume.txt', b'these are the contents of the txt file'))
+    return profile_1
+
+
+class TestProfileModel:
+    # Testing different Personal Profile attributes
+
+    def test_newprofile(self, profile_1, user_1):
+        # Testing Personal Profile data
+        assert profile_1.company == 'Test Company'
+        assert profile_1.user == user_1
+        assert profile_1.about == 'Test About'
+        assert profile_1.birth_date == '2006-10-25'
+        assert profile_1.profile_pic != None
+        assert profile_1.resume != None
