bbdff9c86b09bd46585984539918a833b53f72f6
ShirleyFichman <shirleyfichman@gmail.com>
2022-03-30 19:38:46 +0300

    correcting the default birth date

--- personal_profile/models.py
+++ personal_profile/models.py
@@ -13,7 +13,7 @@ class PersonalProfile(models.Model):
     company = models.CharField(max_length=100)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     about = models.TextField()
-    birth_date = datetime.date(2000, 20, 10)
+    birth_date = datetime.date(2000, 12, 10)
     profile_pic = models.ImageField(null=True, blank=True)
     resume = models.FileField(null=True, upload_to='resumes')
 
