3b0ac27ebfb4f729bca5743428e91588d87e88d9
ShirleyFichman <shirleyfichman@gmail.com>
2022-04-05 13:34:12 +0300

    adding delete function to model

--- personal_profile/models.py
+++ personal_profile/models.py
@@ -1,6 +1,8 @@
 # from tkinter.tix import Tree
+import os
 from django.db import models
 from django.contrib.auth.models import User
+from app.settings import MEDIA_ROOT
 
 
 class ProfileManager(models.Manager):
@@ -15,6 +17,27 @@ class PersonalProfile(models.Model):
     birth_date = models.DateField(null=True)
     profile_pic = models.ImageField(null=True, blank=True, upload_to='profile_pics')
     resume = models.FileField(null=True, upload_to='resumes')
+
+    def delete(self):
+
+        # Get the media root path, and remove the first `/vagrant` folder.
+        media_root_path = os.path.join(*(MEDIA_ROOT.split(os.path.sep)[2:]))
+
+        file_paths_to_delete = [media_root_path + "/" + self.profile_pic.name,
+                                media_root_path + "/" + self.resume.name]
+
+        # Filter list.
+        file_paths_to_delete = [i for i in file_paths_to_delete if
+                                i is not None]
+
+        # Delete files from the backend.
+        if file_paths_to_delete:
+            for file_path in file_paths_to_delete:
+                print(f'deleting {file_path}')
+                os.remove(file_path, dir_fd=None)
+
+        # Call Django's ORM delete this record.
+        super(PersonalProfile, self).delete()
 
     def __str__(self):
         return f'{self.user.username} Profile'
