40f25e6b12782edd71d03ed9661408a025250130
paOmer <omerpahima7@gmail.com>
2022-04-10 09:49:16 +0300

    Setting create post view
    Setting up the post creation view using django CreateView build-in class.
    Setting the post author to be the logged in user.
    Requiring a logged in user in order to create new post.

--- feed/views.py
+++ feed/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import CreateView
 from .models import Post
 
 
@@ -7,3 +9,12 @@ def feed(request):
         'posts': Post.posts.main_feed()
     }
     return render(request, 'feed/feed.html', context)
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
