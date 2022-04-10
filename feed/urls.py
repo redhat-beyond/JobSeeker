088a34df736ae5ae03aa322d81a64c2eaa26763a
paOmer <omerpahima7@gmail.com>
2022-04-10 09:53:47 +0300

    Adding the new post page url

--- feed/urls.py
+++ feed/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from .views import PostCreateView
 from . import views
 
 
 urlpatterns = [
     path('', views.feed, name='feed'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
 ]
