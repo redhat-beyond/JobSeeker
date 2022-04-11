173e55abab08b3bc4cd5e034f26dba655f6860ca
Dean <dinbiton100@gmail.com>
2022-04-11 18:12:06 +0300

    Preference model: modify years_of_experiences and add work_schedule
    - Deleted the YearsOfExperiences model
    - Made 2 changes to the Preference model:
        Modified years_of_experience to a CharField with choices (form of an enum).
        Added work_schedule which is also a CharField with choices (form of an enum).

--- job_board/models/preference.py
+++ job_board/models/preference.py
@@ -1,13 +1,34 @@
 from django.db import models
 from .job_type import JobType
 from .location import Location
-from .years_of_experience import YearsOfExperience
 
 
 class Preference(models.Model):
+
+    class yearsOfExperience(models.TextChoices):
+        NOEXPERIENCE = "no experience", "no experience"
+        YEARS0TO2 = "0-2 years", "0-2 years"
+        YEARS3TO5 = "3-5 years", "3-5 years"
+        YEARS5ANDABOVE = "5+ years", "5+ years"
+
+    class workSchedule(models.TextChoices):
+        NOTSPECIFIED = "Not specified", "Not specified"
+        FULLTIME = "Full-time", "Full-time"
+        PARTTIME = "Part-time", "Part-time"
+        FLEXIBLE = "Flexible", "Flexible"
+
     job_type = models.ForeignKey(JobType, on_delete=models.PROTECT)
     location = models.ForeignKey(Location, on_delete=models.PROTECT)
-    years_of_experience = models.ForeignKey(YearsOfExperience, on_delete=models.PROTECT)
+    years_of_experience = models.CharField(
+        max_length=30,
+        choices=yearsOfExperience.choices,
+        default=yearsOfExperience.NOEXPERIENCE
+    )
+    work_schedule = models.CharField(
+        max_length=30,
+        choices=workSchedule.choices,
+        default=workSchedule.NOTSPECIFIED
+    )
 
     def __str__(self):
         return str(self.id)
