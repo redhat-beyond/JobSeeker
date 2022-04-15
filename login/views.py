0654c23965ad1a0c50488c8323fc938fb4680a8f
Amit Cohen <amitcohen765@gmail.com>
2022-04-15 13:12:50 +0300

    added logout view

--- login/views.py
+++ login/views.py
@@ -5,3 +5,7 @@ from django.contrib.auth.decorators import login_required
 @login_required  # to be changed
 def profile(request):
     return render(request, 'login.html')
+
+@login_required
+def logout_view(request):
+    logout(request)
