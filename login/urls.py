2e50a5b862f9fa5a08dca7b47aa06d84949a85b8
Amit Cohen <amitcohen765@gmail.com>
2022-04-15 13:19:27 +0300

    fixed view

--- login/urls.py
+++ login/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from . import views
 
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', login_view, name='logout'),
+    path('logout/', views.logout_view, name='logout'),
 ]
