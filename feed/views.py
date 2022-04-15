c009ff7cf85f20494adce19212c2fa3548947012
paOmer <omerpahima7@gmail.com>
2022-04-16 00:58:58 +0300

    Setting up the like view

--- feed/views.py
+++ feed/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Post, Comment
 from django.views.generic import DetailView, DeleteView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib import messages
 from django.urls import reverse
+from django.http import HttpResponseRedirect
 
 
 def feed(request):
@@ -59,3 +60,22 @@ class CommentCreateView(LoginRequiredMixin, CreateView):
         comment.save()
 
         return super().form_valid(form)
+
+
+def LikeView(request, pk):
+    post = get_object_or_404(Post, id=pk)
+    user = request.user
+    if user.is_authenticated:
+        if post.likes.filter(id=user.id).exists():
+            post.likes.remove(user)
+        else:
+            post.likes.add(user)
+
+        origin_url = request.META.get('HTTP_REFERER')
+        if origin_url is not None:
+            return HttpResponseRedirect(origin_url)
+        return HttpResponseRedirect(reverse('post-detail', args=(str(pk),)))
+
+    else:
+        messages.warning(request, "You must login in order to like a post")
+        return redirect('login')
