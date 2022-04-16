d5c4349361ff40f6646e8dacffc316c6457e26a0
Amit Cohen <amitcohen765@gmail.com>
2022-04-16 20:04:57 +0300

    removed unused content of views file in login app

--- login/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-
-
-@login_required  # to be changed
-def profile(request):
-    return render(request, 'login.html')
