74a59cab28979c641d282f5fdf585e71d7d8dcca
Dean <dinbiton100@gmail.com>
2022-04-18 01:33:45 +0300

    Preference model: fix enums and set null values

--- job_board/models/preference.py
+++ job_board/models/preference.py
@@ -6,23 +6,24 @@ from .location import Location
 class Preference(models.Model):
 
     class yearsOfExperience(models.TextChoices):
-        NOEXPERIENCE = "no experience", "no experience"
-        YEARS0TO2 = "0-2 years", "0-2 years"
-        YEARS3TO5 = "3-5 years", "3-5 years"
-        YEARS5ANDABOVE = "5+ years", "5+ years"
+        NOTSPECIFIED = "Not specified"
+        NOEXPERIENCE = "no experience"
+        YEARS0TO2 = "0-2 years"
+        YEARS3TO5 = "3-5 years"
+        YEARS5ANDABOVE = "5+ years"
 
     class workSchedule(models.TextChoices):
-        NOTSPECIFIED = "Not specified", "Not specified"
-        FULLTIME = "Full-time", "Full-time"
-        PARTTIME = "Part-time", "Part-time"
-        FLEXIBLE = "Flexible", "Flexible"
+        NOTSPECIFIED = "Not specified"
+        FULLTIME = "Full-time"
+        PARTTIME = "Part-time"
+        FLEXIBLE = "Flexible"
 
-    job_type = models.ForeignKey(JobType, on_delete=models.PROTECT)
-    location = models.ForeignKey(Location, on_delete=models.PROTECT)
+    job_type = models.ForeignKey(JobType, on_delete=models.PROTECT, null=True)
+    location = models.ForeignKey(Location, on_delete=models.PROTECT, null=True)
     years_of_experience = models.CharField(
         max_length=30,
         choices=yearsOfExperience.choices,
-        default=yearsOfExperience.NOEXPERIENCE
+        default=yearsOfExperience.NOTSPECIFIED
     )
     work_schedule = models.CharField(
         max_length=30,
