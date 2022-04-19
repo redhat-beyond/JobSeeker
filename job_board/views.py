2dc2089168994349bd7890bf79b7e94c963282a8
Dean <dinbiton100@gmail.com>
2022-04-20 01:08:14 +0300

    Add the results view to the job_board.html

--- job_board/views.py
+++ job_board/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.edit import FormView
 from .search_form import SearchForm
+from feed.models.post import Post
 
 
 class AddSearchView(FormView):
@@ -22,6 +23,6 @@ class AddSearchView(FormView):
     def post(self, request, *args, **kwargs):
         form = self.form_class(request.POST)
         if form.is_valid():
-            context = {'title': 'Job Board', 'results': form.cleaned_data, 'form': form}
+            context = {'title': 'Job Board', 'results': Post.posts.filter(is_job_offer=True), 'form': form}
 
         return render(request, 'job_board/job_board.html', context)
