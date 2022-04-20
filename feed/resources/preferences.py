ecd5f41ba441dfae01bfcf04af40916af2c916bc
Dean <dinbiton100@gmail.com>
2022-04-20 07:18:22 +0300

    Update preference data migrations

--- feed/resources/preferences.py
+++ feed/resources/preferences.py
@@ -1,4 +1,4 @@
 PREFERENCES = [
-    ['Delivery driver', 'Haifa', 'no experience', 'Flexible'],
+    ['Delivery driver', 'Haifa', 'No experience', 'Flexible'],
     ['Web developer', 'Tel Aviv', '5+ years', 'Full-time'],
 ]
