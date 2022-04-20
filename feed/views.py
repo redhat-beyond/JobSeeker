2098c698ee40b2ee2dfc02f725a9e57f30f703cf
paOmer <omerpahima7@gmail.com>
2022-04-20 23:14:11 +0300

    Setting up the comment create view

--- feed/views.py
+++ feed/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .models import Post
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Comment
 from django.views.generic import DetailView, DeleteView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib import messages
@@ -41,4 +41,21 @@ class PostCreateView(LoginRequiredMixin, CreateView):
         post = form.save(commit=False)
         post.author = self.request.user
         post.save()
+        return super().form_valid(form)
+
+
+class CommentCreateView(LoginRequiredMixin, CreateView):
+    model = Comment
+    fields = ['content']
+    login_url = '/login/'
+
+    def form_valid(self, form):
+        post_parent_id = self.kwargs.get('pk')
+        post_parent = get_object_or_404(Post, id=post_parent_id)
+
+        comment = form.save(commit=False)
+        comment.author = self.request.user
+        comment.post_parent = post_parent
+        comment.save()
+
         return super().form_valid(form)
