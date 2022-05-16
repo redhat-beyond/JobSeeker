62e288937ebbbbdae334555aed0a6b9953473f62
ShirleyFichman <shirleyfichman@gmail.com>
2022-05-16 13:06:28 +0300

    initializing the update profile form

--- personal_profile/urls.py
+++ personal_profile/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('profile/<int:pk>/', views.ProfileDetailView.as_view(), name='profile-detail'),
+    path('update_profile/', views.updateProfile, name='update_profile')
 ]
--- personal_profile/views.py
+++ personal_profile/views.py
@@ -1,6 +1,11 @@
 from django.views.generic import DetailView
 from .models import PersonalProfile
+from django.shortcuts import render
 
 
 class ProfileDetailView(DetailView):
     model = PersonalProfile
+
+def updateProfile(request):
+    context= {}
+    return render(request, 'templates/personal_profile/update_form.html')
