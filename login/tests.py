ed541bb06299577dc1cf9d9f9a91e51946216b25
Amit Cohen <amitcohen765@gmail.com>
2022-05-17 19:48:02 +0300

    added logout test

--- login/tests.py
+++ login/tests.py
@@ -1,3 +1,10 @@
 def test_login_app_entrypoint(client):
     response = client.get("/login/")
     assert response.status_code == 200
+
+def test_logout_link(self, client, user):
+        client.force_login(user)
+        response = client.get('/logout/')
+        response = client.get(response.url)
+        assert response.context['user'].is_anonymous
+        
