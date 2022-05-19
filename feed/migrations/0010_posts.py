5bc000b3be1ebb2c1abd920feb5b827789d60a27
ShirleyFichman <shirleyfichman@gmail.com>
2022-05-19 12:46:16 +0300

    amending the posts data file to json

--- feed/migrations/0010_posts.py
+++ feed/migrations/0010_posts.py
@@ -1,10 +1,11 @@
 from django.db import migrations
 from feed.models.post import Post
 from job_board.models.preference import Preference, JobType
-from feed.resources.posts import POSTS
+# from feed.resources.posts import POSTS
 from job_board.resources.locations import LOCATIONS
 import django.contrib.auth
 User = django.contrib.auth.get_user_model()
+import json
 
 
 class Migration(migrations.Migration):
@@ -25,15 +26,19 @@ class Migration(migrations.Migration):
                 prefernces=Preference.objects.filter(id=i).first()
             )
 
-        for post in POSTS:
+        f = open('feed/resources/posts.json')
+        data = json.load(f)
+
+        for post in data['POSTS']:
             Post.posts.create(
-                title=post[0],
-                content=post[1],
-                author=User.objects.filter(username=post[2]).first(),
-                is_job_offer=post[3],
-                prefernces=Preference.objects.filter(job_type=JobType.objects.filter(text=post[4]).first()).first()
+                title=post['title'],
+                content=post['content'],
+                author=User.objects.filter(username=post['author']).first(),
+                is_job_offer=post['is_job_offer'],
+                prefernces=Preference.objects.filter(job_type=JobType.objects.filter(text=post['prefernces']).first()).first()
             ).save()
 
     operations = [
         migrations.RunPython(generate_posts),
     ]
+
